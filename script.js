45b87e0a09f3cbda327294c4fb2e3bff7d6213cc
lepinvlad <lepinvladislavbb@gmail.com>
2024-11-01 12:21:52 +0300

    realize game logic

--- script.js
+++ script.js
@@ -15,3 +15,53 @@ function makeRowsAndCols(rowsNum, colsNum) {
 }
 
 makeRowsAndCols(rowsNum, colsNum);
+
+// Game Bar
+const gameBar = document.createElement("div");
+gameBar.classList.add("game_bar");
+
+const gameBarHeader = document.createElement("h1");
+gameBarHeader.classList.add("game_bar_header");
+gameBarHeader.textContent = "Etch-A-Sketch";
+gameBar.appendChild(gameBarHeader);
+
+const playButton = document.createElement("button");
+playButton.classList.add("play_button");
+playButton.textContent = "PLAY";
+gameBar.appendChild(playButton);
+
+const resetButton = document.createElement("button");
+resetButton.classList.add("reset_button");
+resetButton.textContent = "RESET";
+gameBar.appendChild(resetButton);
+
+container.appendChild(gameBar);
+
+// Game Logic
+let isPlaying = false;
+
+const allDivs = container.querySelectorAll("div");
+const rows = [];
+
+allDivs.forEach((div) => {
+  const row = [...div.getElementsByClassName("grid")];
+  rows.push(...row);
+});
+
+playButton.addEventListener("click", () => {
+  alert("Okey, try it");
+
+  rows.forEach((row) => {
+    row.addEventListener("mouseover", () => {
+      row.style.backgroundColor = "red";
+    });
+  });
+});
+
+resetButton.addEventListener("click", () => {
+  alert("Okey, Got u");
+
+  rows.forEach((row) => {
+    row.style.backgroundColor = "white";
+  });
+});
